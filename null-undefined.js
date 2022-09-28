050bd41c3b87d542fea6cc1b877d827c85ce8dbc
ashraful075 <ashraful075@gmail.com>
2022-09-28 17:03:59 +0600

    null

---
+++ null-undefined.js
@@ -0,0 +1,14 @@
+//let pakhi;
+//function add(num1,num2){
+    //console.log(num1+num2)
+    return
+//}
+//function add(num1,num2){
+    //console.log(num1,num2);
+//}
+//const premik = {name:"smat dude",phone:5675};
+//console.log(premik.gf);
+let fun = undefined;
+console.log(fun);
+let ages =[2,3,5]
+console.log(ages[11]);
